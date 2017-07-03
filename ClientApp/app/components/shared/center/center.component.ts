a517b251195211c55c6f09adc677b6daf25ef372
yanivnoema <yaniv19200@hotmail.co.il>
2017-07-03 20:25:02 +0300

    Add new project for the server and add angular2 objects

--- ClientApp/app/components/shared/center/center.component.ts
+++ ClientApp/app/components/shared/center/center.component.ts
@@ -2,21 +2,41 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
 class Url {
 
-    address: string
-    prettyUrl: string;
+    Address: string;
+    IsRemoved: boolean;
 
-    constructor(add: string) {
-        this.address = add;
-         
-    }
+    constructor(Input: string) {
 
-    UrlFilter(unUrl: string): void {
-        
-    }
+        this.Address = Input;
+        this.IsRemoved = false;
+
+    }   
 }
 
+class Phrase {
+
+    Words: string[] = [];
+    IsRemoved: boolean;
+
+    constructor(Phrase: string) {
+
+        this.Words = Phrase.split(' ');
+        this.IsRemoved = false;
+    }
+
+}
+
+class Searches {
+
+    Url: Url;
+    PharseList: Phrase[] = [];
+    IsCaseSensative: boolean;
+    IsExcatMatch: boolean;
+
+}
 
 
 @Component({
@@ -24,19 +44,43 @@ class Url {
     styleUrls: ['./center.component.css'],
     templateUrl: './center.component.html'
 })
+
 export class CenterComponent {
 
-    str: string;
-    urlList: Url[] = [];
-    toggle: boolean = false;
+    URL: string;
+    Pharse: string;
+    UrlList: Url[] = [];
+    PhraseList: Phrase[] = [];
+    SearchesList: Searches[] = [];
 
-    sendUrlForDeepSearching(): void {
-        if (this.str != undefined) {
-            this.urlList.push(new Url(this.str));
-            this.str = "http://"
+    //toggle: boolean = false;
+    // update placeholder after each insertion
+
+    AddURL(): void {
+
+        if (this.URL != undefined) {
+            this.UrlList.push(new Url("http://" + this.URL));
+            this.URL = "http://";
         }
+    }
 
-        if (this.urlList.length > 0)
-            this.toggle = true;
+    AddPharse(): void {
+
+        if (this.Pharse != undefined) {
+            this.PhraseList.push( new Phrase(this.Pharse));
+            this.Pharse = "Add more phrase";
+        }
+    }
+
+    Search(): void {
+
+    }
+
+    RemoveDiv(U: Url) {
+        U.IsRemoved = true;
+        
     }
 }
+
+
+    
